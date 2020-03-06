55ac729d3a47b4fee678b3849768705ee521b988
vkshay3400 <akshayp2208@gmail.com>
2020-03-06 12:36:18 +0530

    Added: Email Id

--- src/main/java/UserRegistrationProblem.java
+++ src/main/java/UserRegistrationProblem.java
@@ -4,6 +4,7 @@ public class UserRegistrationProblem {
     //DECLARE PATTERN
     String firstNamePattern="^[A-Z][a-z]{2,}$";
     String lastNamePattern="^[A-Z][a-z]{2,}$";
+    String emailId="^[a-z]+[.]?([-+0-9]*)?([a-z0-9]+)+[@]+(([a-z]{3,5})|([0-9]))+[.]+[a-z]{2,3}+(([.]+[a-z]{2,3})*)$";
 
     //MAIN METHOD
     public static void main(String[] args) {
@@ -18,5 +19,10 @@ public class UserRegistrationProblem {
     //METHOD TO VALIDATE LAST NAME
     public boolean validateLastName(String lastName){
         return (Pattern.matches(lastNamePattern,lastName));
+    }
+
+    //METHOD FOR EMAIL ID
+    public boolean validateEmailId(String email){
+        return (Pattern.matches(emailId,email));
     }
 }
--- src/test/java/UserRegistrationProblemTest.java
+++ src/test/java/UserRegistrationProblemTest.java
@@ -2,36 +2,83 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationProblemTest {
-    UserRegistrationProblem validate=new UserRegistrationProblem();
+    UserRegistrationProblem validate = new UserRegistrationProblem();
+    String validateEmail[] = {
+            "abc@yahoo.com",
+            "abc-100@yahoo.com",
+            "abc.100@yahoo.com",
+            "abc111@abc.com",
+            "abc-100@abc.net",
+            "abc.100@abc.com.au",
+            "abc@1.com",
+            "abc@gmail.com.com",
+            "abc+100@gmail.com"
+    };
+    String invalidateEmail[] = {
+            "abc",
+            "abc@.com.my",
+            "abc123@gmail.a",
+            "abc123@.com",
+            "abc123@.com.com",
+            ".abc@abc.com",
+            "abc()*@gmail.com",
+            "abc@%*.com",
+            "abc..2002@gmail.com",
+            "abc.@gmail.com",
+            "abc@abc@gmail.com",
+            "abc@gmail.com.1a",
+            "abc@gmail.com.aa.au"
+    };
+
     //TEST CASE TO CHECK FIRST NAME VALID
     @Test
-    public void givenFirstName_whenValid_thenReturn(){
-        String fname="Akshay";
+    public void givenFirstName_whenValid_thenReturn() {
+        String fname = "Akshay";
         boolean result = validate.validateFirstName(fname);
         Assert.assertTrue(result);
     }
 
     //TEST CASE TO CHECK FIRST NAME INVALID
     @Test
-    public void givenFirstName_whenInvalid_thenReturn(){
-        String fname="akshay";
+    public void givenFirstName_whenInvalid_thenReturn() {
+        String fname = "akshay";
         boolean result = validate.validateFirstName(fname);
         Assert.assertFalse(result);
     }
 
     //TEST CASE TO CHECK LAST NAME VALID
     @Test
-    public void givenLastName_whenValid_thenReturn(){
-        String fname="Power";
+    public void givenLastName_whenValid_thenReturn() {
+        String fname = "Power";
         boolean result = validate.validateLastName(fname);
         Assert.assertTrue(result);
     }
 
     //TEST CASE TO CHECK LAST NAME INVALID
     @Test
-    public void givenLastName_whenInvalid_thenReturn(){
-        String fname="power";
+    public void givenLastName_whenInvalid_thenReturn() {
+        String fname = "power";
         boolean result = validate.validateLastName(fname);
         Assert.assertFalse(result);
+    }
+
+    //TEST CASE TO CHECK EMAIL ID VALID
+    @Test
+    public void givenEmailId_whenValid_thenReturn() {
+        for (int index=0; index<validateEmail.length-1; index++) {
+            String email = validateEmail[index];
+            boolean result = validate.validateEmailId(email);
+            Assert.assertTrue(result);
+        }
+    }
+
+    //TEST CASE TO CHECK EMAIL ID INVALID
+    @Test
+    public void givenEmailId_whenInvalid_thenReturn() {
+        for (int index=0; index<invalidateEmail.length-1; index++) {
+            String email = invalidateEmail[index];
+            boolean result = validate.validateEmailId(email);
+            Assert.assertFalse(result);
+        }
     }
 }
